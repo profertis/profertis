d1be2bcdcee8a9024cd63e7eb6c5bdcd97b9d671
Tristan F <LeoDog896@hotmail.com>
2022-09-13 16:29:00 +0000

    Base logged in

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    export let loggedIn = false;
+</script>
+
+{#if loggedIn}
+    <h1>Welcome!</h1>
+{:else}
+    <input placeholder="Username">
+    <input placeholder="Password">
+{/if}
