dbf1330de235e5fa14c3010e8a879322c2e30075
Tristan F <LeoDog896@hotmail.com>
2022-09-13 22:19:14 +0000

    deno!

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,17 @@
-import Surreal from 'surrealdb.js';
+import Surreal from "https://deno.land/x/surrealdb@v0.2.0/mod.ts";
 
-const db = new Surreal('http://127.0.0.1:8000/rpc');
+interface Location {
+    zipCode: string;
+    city: string;
+    state: string;
+}
+
+interface School {
+    name: string;
+    location: string;
+}
+
+const db = new Surreal('http://127.0.0.1:8000/rpc', {});
 
 async function main() {
     await db.signin({
