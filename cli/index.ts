479eaa9385c7a9c50b99bb86f8ce66d1b72167d8
Tristan F <LeoDog896@hotmail.com>
2022-09-15 00:03:54 +0000

    CLI tool for creating districts & schools

--- cli/index.ts
+++ cli/index.ts
@@ -1,27 +1,127 @@
 import Surreal from "https://deno.land/x/surrealdb@v0.2.0/mod.ts";
+import {
+  Input,
+  Select,
+} from "https://deno.land/x/cliffy@v0.25.1/prompt/mod.ts";
 import { queries } from "./setup.ts";
 
-const db = new Surreal('http://localhost:8000/rpc', {});
+const db = new Surreal("http://localhost:8000/rpc", {});
 
-async function main() {
-    console.debug("Logging in...")
-    await db.signin({
-        user: 'root',
-        pass: 'root',
+function getRandomString(s: number) {
+  if (s % 2 == 1) {
+    throw new Deno.errors.InvalidData("Only even sizes are supported");
+  }
+  const buf = new Uint8Array(s / 2);
+  crypto.getRandomValues(buf);
+  let ret = "";
+  for (let i = 0; i < buf.length; ++i) {
+    ret += ("0" + buf[i].toString(16)).slice(-2);
+  }
+  return ret;
+}
+
+async function applyDefaultQueries(database: Surreal) {
+  for (const query of queries) {
+    await database.query(query.replaceAll("\n", " ").replace("	", " "), {});
+  }
+}
+
+async function prompt(database: Surreal) {
+  const action = await Select.prompt({
+    message: "Create, list, or update schema?",
+    options: ["create", "list", "schema"],
+  });
+
+  if (action == "create") {
+    const type = await Select.prompt({
+      message: "Create a disctrict a or school?",
+      options: ["district", "school"],
     });
 
-    await db.wait();
-    
-    console.debug("Logged in to SurrealDB.")
+    if (type == "school") {
+      const districts = await database.select("district");
+      if (districts.length === 0) {
+        console.log("No districts found. Create one first!");
+        prompt(database);
+      }
 
-    // Select a specific namespace / database
-	await db.use('profertis', 'profertis');
+      const district = await Select.prompt({
+        message: "District?",
+        options: districts.map((district: { id: string }) => district.id).map((
+          id: string,
+        ) => id.split(":")[1]),
+      });
 
-    console.debug("Using profertis/profertis");
+      const school = await Input.prompt("School Name?");
 
-    for (const query of queries) {
-        await db.query(query.replaceAll("\n", " ").replace("	", " "), {})
+      await database.query(
+        "CREATE type::thing('school', $school) SET district = type::thing('district', $district)",
+        {
+          school,
+          district,
+        },
+      );
+
+      const password = getRandomString(32); // we'll randomly generate a password and allow the admin to change it later on.
+
+      await database.signup({
+        NS: "profertis",
+        DB: "profertis",
+        SC: "admin",
+        username: "superadmin",
+        pass: password
+      });
+
+      console.log("Generated! Username: `superadmin` Password: " + password + ". Send this to the recipient.");
+    } else {
+      const name = await Input.prompt("District name?");
+
+      await database.create("district:" + name, {});
+
+      console.log("Created district " + name + "!");
     }
+  } else if (action == "list") {
+    const type = await Select.prompt({
+      message: "List disctricts or schools?",
+      options: ["district", "school"],
+    });
+
+    const records = await database.select(type);
+
+    console.log(records);
+  } else if (action == "schema") {
+    await applyDefaultQueries(database);
+  }
+
+  await prompt(database);
+}
+
+Deno.addSignalListener("SIGINT", async () => {
+  console.log(
+    "Exiting from prompt loop! (If you actually want to exit, do CTRL + D)",
+  );
+  await prompt(db);
+});
+
+async function main() {
+  console.debug("Logging in...");
+  await db.signin({
+    user: "root",
+    pass: "root",
+  });
+
+  await db.wait();
+
+  console.debug("Logged in to SurrealDB.");
+
+  // Select a specific namespace / database
+  await db.use("profertis", "profertis");
+
+  console.debug("Using profertis/profertis");
+
+  await applyDefaultQueries(db);
+
+  await prompt(db);
 }
 
 main();
--- cli/setup.ts
+++ cli/setup.ts
@@ -2,19 +2,20 @@
 
 // What is this? Why this setup? https://github.com/surrealdb/surrealdb/issues/90 Until this is fixed, each query has to be passed individually.
 const scopes: string[] = [
-    // We're declaring scopes for (admin, teacher, and student) with the same 1d user and pass auth system
-    `DEFINE SCOPE admin SESSION 1d
-        SIGNUP ( CREATE user SET user = $user, pass = crypto::argon2::generate($pass) )
-        SIGNIN ( SELECT * FROM user WHERE user = $user AND crypto::argon2::compare(pass, $pass) )
+  // We're declaring scopes for (admin, teacher, and student) with the same 1d user and pass auth system
+  `DEFINE SCOPE admin SESSION 1d
+        SIGNUP ( CREATE admin SET username = $username, pass = crypto::argon2::generate($pass) )
+        SIGNIN ( SELECT * FROM admin WHERE username = $username AND crypto::argon2::compare(pass, $pass) )
 ;`,
-`DEFINE SCOPE teacher SESSION 1d
-        SIGNUP ( CREATE user SET user = $user, pass = crypto::argon2::generate($pass) )
-        SIGNIN ( SELECT * FROM user WHERE user = $user AND crypto::argon2::compare(pass, $pass) )
+  `DEFINE SCOPE teacher SESSION 1d
+        SIGNUP ( CREATE teacher SET username = $username, pass = crypto::argon2::generate($pass) )
+        SIGNIN ( SELECT * FROM teacher WHERE username = $username AND crypto::argon2::compare(pass, $pass) )
 ;`,
-`DEFINE SCOPE student SESSION 1d
-        SIGNUP ( CREATE user SET user = $user, pass = crypto::argon2::generate($pass) )
-        SIGNIN ( SELECT * FROM user WHERE user = $user AND crypto::argon2::compare(pass, $pass) )
-;`]
+  `DEFINE SCOPE student SESSION 1d
+        SIGNUP ( CREATE student SET username = $username, pass = crypto::argon2::generate($pass) )
+        SIGNIN ( SELECT * FROM student WHERE username = $username AND crypto::argon2::compare(pass, $pass) )
+;`,
+];
 
 const permissisons: string[] = [`DEFINE TABLE course SCHEMALESS
     PERMISSIONS
@@ -25,6 +26,6 @@ const permissisons: string[] = [`DEFINE TABLE course SCHEMALESS
             OR $auth.admin = true
             /* And they must be in the same school */
             AND school = $auth.school,
-        FOR create, delete, update WHERE $auth.admin = true;`]
+        FOR create, delete, update WHERE $auth.admin = true;`];
 
-export const queries: string[] = [...scopes, ...permissisons]
+export const queries: string[] = [...scopes, ...permissisons];
